473fa187d1d354439535cc35245ecb44aaf9299c
Ivoine <ivoinestrachan242@gmail.com>
2023-04-03 16:45:36 -0700

    refactoring code

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Background from "../components/Background";
+import Image from "next/image";
+import sledguy from "../public/assets/sled.svg";
 
 interface Score {
   id: string;
@@ -14,7 +16,7 @@ interface Score {
 const Home: NextPage = () => {
   const [topScores, setTopScores] = useState<Score[]>([]);
   useEffect(() => {
-    fetch("https://sinerider-scoring.up.railway.app/all")
+    fetch("https://sinerider-api.herokuapp.com/all")
       .then((response) => response.json())
       .then((data) => {
         const scores = data.scores; // Get the scores array from the response data
@@ -22,13 +24,13 @@ const Home: NextPage = () => {
         const sortedScores = scores.sort(
           (a: Score, b: Score) => a.T - b.T || a.charCount - b.charCount
         );
-        // Take the top 5 scores
-        const top5Scores = sortedScores.slice(0, 5);
+        // Take the top 4 scores
+        const top5Scores = sortedScores.slice(0, 4);
         setTopScores(top5Scores);
       });
   }, []);
 
-  console.log("Top 5 scores:", topScores);
+  console.log("Top 4 scores:", topScores);
 
   return (
     <>
@@ -38,17 +40,21 @@ const Home: NextPage = () => {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className="pt-[100px]">
-          <div className="bg-[rgba(255,255,255,0.4)] h-[850px] md:w-[1200px] ml-auto mr-auto rounded-[12px]">
-            <div className="text-center">
-              <button className="text-center font-bold text-[32px] justify-center bg-white rounded-[8px] pl-20 pr-20 py-2 cursor-none relative bottom-8">
-                Leaderboard
-              </button>
+          <div className="md:w-[1200px] ml-auto mr-auto">
+            <div className="flex items-center gap-10 bg-white sm:h-[117px] h-[90px] ml-2 mr-2 rounded-[12px]  sm:ml-5 sm:mr-5 px-10 mt-5">
+              <div>
+                <Image src={sledguy} width={109} height={116} alt="sled" />
+              </div>
+
+              <div className="font-mono text-[62px]">SinerRider</div>
             </div>
+          </div>
+          <div className="md:w-[1200px] ml-auto mr-auto">
             {topScores.map((score, index) => (
               <div
                 key={score.id}
                 className={`bg-white flex sm:h-[117px] h-[90px] ml-2 mr-2 rounded-[12px] justify-between items-center sm:ml-5 sm:mr-5 px-10 mt-5 ${
-                  index > 0 ? "5" : "0"
+                  index > 0 ? "4" : "0"
                 }`}
               >
                 <div className="font-bold font-mono sm:text-[48px] text-[22px]">
@@ -56,7 +62,7 @@ const Home: NextPage = () => {
                 </div>
                 <div>Player name</div>
                 <div className="sm:text-[52px] text-[22px] font-bold font-mono">
-                  {score.T.toFixed(2)}
+                  {score.T ? score.T.toFixed(2) : ""}
                 </div>
                 <div>image</div>
               </div>
