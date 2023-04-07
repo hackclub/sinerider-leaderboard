9e3cf947e5a46512346df59fbacf587606aeee40
JosiasAurel <ndjosiasaurel@gmail.com>
2023-04-07 16:29:07 +0100

    rename fields and cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,27 +9,35 @@ import sledguy from "../public/assets/sled.svg";
 interface Score {
   id: string;
   expression: string;
-  T: number;
+  time: number;
   level: string;
   charCount: number;
+  player: string
 }
 
 const Home: NextPage = () => {
   const [topScores, setTopScores] = useState<Score[]>([]);
-  const [levels, setLevels] = useState<Set<string>>(new Set()); 
+  const [levels, setLevels] = useState<Set<string>>(new Set());
+
+  async function getScores(level?: string) {
+    let response;
+    if (level) {
+      response = await fetch(`https://sinerider-api.herokuapp.com/level/${level}`)
+    } else {
+      response = await fetch("https://sinerider-api.herokuapp.com/all")
+    }
+
+    const data = await response.json();
+
+    return data.scores.sort((a, b) => (a.time - b.time), 0);
+  }
 
   useEffect(() => {
-    fetch("https://sinerider-api.herokuapp.com/all")
-      .then((response) => response.json())
-      .then((data) => {
-        const scores = data.scores;
-        const sortedScores = scores.sort(
-          (a: Score, b: Score) => a.T - b.T || a.charCount - b.charCount
-        );
-        const top4Scores = sortedScores.slice(0, 4);
+    getScores()
+      .then(scores => {
+        const top4Scores = scores.slice(0, 4);
         setTopScores(top4Scores);
 
-       
         const levels = new Set(scores.map((score: Score) => score.level));
         setLevels(levels);
       });
@@ -41,16 +49,19 @@ const Home: NextPage = () => {
   const handleLevelSelect = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedLevel = event.target.value;
     console.log(`Selected level: ${selectedLevel}`);
-    fetch(`https://sinerider-api.herokuapp.com/level/${selectedLevel}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const scores = data.scores;
-        const sortedScores = scores.sort(
-          (a: Score, b: Score) => a.T - b.T || a.charCount - b.charCount
-        );
-        const top4Scores = sortedScores.slice(0, 4);
+
+    if (selectedLevel.trim().length === 0) {
+      getScores().then(scores => {
+        const top4Scores = scores.slice(0, 4);
         setTopScores(top4Scores);
-      });
+      })
+    } else {
+      getScores(selectedLevel)
+        .then(scores => {
+          const top4Scores = scores.slice(0, 4);
+          setTopScores(top4Scores);
+        })
+    }
   };
 
   return (
@@ -65,15 +76,15 @@ const Home: NextPage = () => {
             <div className="flex items-center gap-10 bg-white sm:h-[117px] h-[90px] ml-2 mr-2 rounded-[12px]  sm:w-full w-[90%] justify-between px-5 py-3">
               <div className="flex items-center gap-10">
                 <div>
-              <Image src={sledguy} alt="sled" height={90} width={90} />
-              </div>
-              <div className="font-mono sm:text-[54px] text-[22px]">
-              SineRider
-              </div>
+                  <Image src={sledguy} alt="sled" height={90} width={90} />
+                </div>
+                <div className="font-mono sm:text-[54px] text-[22px]">
+                  SineRider
+                </div>
               </div>
               <div>
                 <select
-                  
+
                   onChange={handleLevelSelect}
                 >
                   <option value="">All time</option>
@@ -90,22 +101,21 @@ const Home: NextPage = () => {
             {topScores.map((score, index) => (
               <div
                 key={score.id}
-                className={`bg-white flex sm:h-[117px] h-[90px] ml-2 mr-2 rounded-[12px] justify-between items-center sm:ml-5 sm:mr-5 px-10 mt-5 ${
-                  index > 0 ? "4"
- : "0"
-                }`}
+                className={`bg-white flex sm:h-[117px] h-[90px] ml-2 mr-2 rounded-[12px] justify-between items-center sm:ml-5 sm:mr-5 px-10 mt-5 ${index > 0 ? "4"
+                  : "0"
+                  }`}
               >
                 <div className="font-bold font-mono sm:text-[48px] text-[22px]">
                   {index + 1}
                 </div>
-                <div>Player name</div>
+                <div>{score.player.length > 0 ? score.player : "NO_NAME"}</div>
                 <div> </div> <span>plays</span>
-                <div></div>{score.charCount} <span>char</span>
+                <div></div> <span>{score.charCount} characters</span>
                 <div className="flex items-center">
-                <div className="sm:text-[52px] text-[22px] font-bold font-mono">
-                {score.T ? parseInt((score.T * 60).toString()) : ""}
-                </div>
-                <span >seconds</span>
+                  <div className="sm:text-[52px] text-[22px] font-bold font-mono">
+                    {score.time ? parseInt((score.time * 60).toString()) : ""}
+                  </div>
+                  <span >seconds</span>
                 </div>
               </div>
             ))}
