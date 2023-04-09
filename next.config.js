c9bbd701581a1211c40ec2127d8f59dda7a564ce
Graham Darcey <graham@hackclub.com>
2023-04-08 23:16:03 -0400

    UI and functionality improvements

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+const webpack = require('webpack')
+const { parsed: myEnv } = require('dotenv').config({
+  path:'.env'
+})
+
 module.exports = {
+  webpack(config) {
+    config.plugins.push(new webpack.EnvironmentPlugin(myEnv))
+    return config
+  },
   reactStrictMode: true,
-  
+  //compress: false  
 }
