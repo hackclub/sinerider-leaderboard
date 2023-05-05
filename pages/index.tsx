05db22a423af9db272f738394f7e285b707fe815
Ivoine <ivoinestrachan242@gmail.com>
2023-05-04 18:48:06 -0700

    fix some changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -265,6 +265,13 @@ const Home: NextPage = () => {
             ))}
           </div>
         </div>
+        <footer className="text-center text-white bottom-2 fixed flex w-full justify-center">
+          <a href="https://github.com/hackclub/sinerider-leaderboard#readme">
+          <div className="text-center ">
+          Built and maintained by teenagers. Version 0.5
+          </div>
+          </a>
+          </footer>
       </Background>
     </>
   );
